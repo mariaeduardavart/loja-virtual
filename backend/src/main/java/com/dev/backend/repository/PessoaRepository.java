0954ad0ccb01540248f28a6b4a485eb8e4ae1535
mariaeduardavart <dudavart@gmail.com>
2023-07-14 18:20:42 -0300

    recuperação de senha

--- backend/src/main/java/com/dev/backend/repository/PessoaRepository.java
+++ backend/src/main/java/com/dev/backend/repository/PessoaRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dev.backend.entity.Pessoa;
 
+
+
 public interface PessoaRepository  extends JpaRepository<Pessoa, Long>{
     
+    Pessoa findByEmail(String email);
+    Pessoa findByEmailAndCodigoRecuperacaoSenha(String email, String codigoRecuperacaoSenha);
+
+
 }
