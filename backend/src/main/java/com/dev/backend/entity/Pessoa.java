d368315b874132f0b2d64609aaa9dd258339fce6
mariaeduardavart <dudavart@gmail.com>
2023-07-14 14:09:27 -0300

    crud permissao pessoa + alterações em pessoa

--- backend/src/main/java/com/dev/backend/entity/Pessoa.java
+++ backend/src/main/java/com/dev/backend/entity/Pessoa.java
@@ -2,14 +2,20 @@ package com.dev.backend.entity;
 
 import java.util.Date;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
@@ -35,11 +41,22 @@ public class Pessoa {
     @JoinColumn(name = "idCidade")
     private Cidade cidade;
 
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.MERGE}) //passa para o mapeamento o nome do atributo
+    @Setter(value = AccessLevel.NONE) // desabilitando o setter que o lombok gera
+    private List<PermissaoPessoa> permissaoPessoa;
+
     //Armazenar data e hora no banco
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
+
+    public void setPermissaoPessoa(List<PermissaoPessoa> pp){
+        for(PermissaoPessoa p:pp){
+            p.setPessoa(this);
+        }
+        this.permissaoPessoa = pp;
+    }
 
     
 }
